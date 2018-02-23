8274c5311cdc23290c0ed33b67079614291de6c4
root <root@adonyec1.mylabserver.com>
2018-02-23 00:42:41 +0000

    add 2 files

---
+++ rs_pe.sh
@@ -0,0 +1,7 @@
+service pe-puppetdb stop
+service pe-puppetserver stop
+service pe-console-services stop
+service pe-puppetdb start
+service pe-puppetserver start
+service pe-console-services start
+
